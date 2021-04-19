656fcea4e3f326e69e0a21aa8811b906ed3a1f2d
atablecompany <tenglados@gmail.com>
2021-04-19 10:11:36 +0200

    Hotovy prvni ukol

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,9 +14,17 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    with open(file_path) as f:
+        data = json.load(f)
+        if field in data.keys():
+            return data[field]
+        else:
+            return None
+
 
 def main():
-    pass
+    sequential_data = read_data(file_name='sequential.json', field="dna_sequence")
+    print(sequential_data)
 
 
 if __name__ == '__main__':
